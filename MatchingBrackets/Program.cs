f95c7cf2cc91ed0f0344e46b8bb9d960a754e18f
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-14 18:15:59 +0300

    Stacks exercise

---
+++ MatchingBrackets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> indexes = new Stack<int>();
+
+            foreach (char ch in input)
+            {
+                if (ch == '(')
+                {
+                    indexes.Push(input.IndexOf(ch));
+                }
+
+                if (ch == ')')
+                {
+                    int index = indexes.Pop();
+                    string subexpression = input.Substring(index, input.IndexOf(')') - index + 1);
+
+                    Console.WriteLine(subexpression);
+                }
+            }
+        }
+    }
+}
