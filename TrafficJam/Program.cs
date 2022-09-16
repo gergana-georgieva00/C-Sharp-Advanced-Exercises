1350810740ef8ccdd1a8445d38235dfb00ddb64f
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-16 17:07:27 +0300

    Queue exercise

---
+++ TrafficJam/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>();
+            int passedCars = 0;
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queue.Count > 0)
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            passedCars++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}
