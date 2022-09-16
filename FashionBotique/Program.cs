2994c0beea9391b6e0a4ef5622e0766da0ee80fe
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-16 23:57:12 +0300

    Stack and queue homework

---
+++ FashionBotique/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBotique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+            int capacityCopy = capacity;
+
+            Stack<int> stack = new Stack<int>(integers);
+            int sum = 0;
+            int racks = 1;
+
+            if (capacity == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            while (stack.Count > 0)
+            {
+                if (capacityCopy - stack.Peek() == 0 && stack.Count > 0)
+                {
+                    capacityCopy = capacity;
+                    stack.Pop();
+
+                    if (stack.Count > 0)
+                    {
+                        racks++;
+                    }
+                }
+                else if (capacityCopy - stack.Peek() < 0 && stack.Count > 0)
+                {
+                    capacityCopy = capacity;
+
+                    if (stack.Count > 0)
+                    {
+                        racks++;
+                    }
+                }
+                else
+                {
+                    capacityCopy -= stack.Pop();
+                }
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
