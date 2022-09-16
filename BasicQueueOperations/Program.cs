70a66f5a652e6b48e09e05c325dd89f849d75aca
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-16 19:45:03 +0300

    Stack and queue homework

---
+++ BasicQueueOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            int[] integers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(integers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+        }
+    }
+}
