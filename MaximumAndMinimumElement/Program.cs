e251b83ca8e2f946e5af659726214e9795c3ab79
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-16 21:32:50 +0300

    Stack and queue homework

---
+++ MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] query = Console.ReadLine().Split();
+
+                if (query[0] == "1")
+                {
+                    stack.Push(int.Parse(query[1]));
+                }
+                else if (query[0] == "2")
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (query[0] == "3")
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                else
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
