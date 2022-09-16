2e1ce8232deb345a3df6d3d51ca531b80e1d4c0c
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-16 18:09:09 +0300

    Queue exercise

---
+++ PrintEvenNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            List<int> evenNums = new List<int>();
+
+            while (queue.Count > 0)
+            {
+                if (queue.Peek() % 2 == 0)
+                {
+                    evenNums.Add(queue.Dequeue());
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenNums));
+        }
+    }
+}
