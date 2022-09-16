dbbea13ded1e39af05842b7a30577e175fbf9e7a
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-16 17:55:43 +0300

    Stack exercise

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Array.Reverse(input);
+
+            Stack<string> stack = new Stack<string>(input);
+
+            int result = int.Parse(stack.Pop());
+
+            while (stack.Count > 0)
+            {
+                string currOperator = stack.Pop();
+                int currNum = int.Parse(stack.Pop());
+
+                if (currOperator == "+")
+                {
+                    result += currNum;
+                }
+                else
+                {
+                    result -= currNum;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
