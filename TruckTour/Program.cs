8f0d361da658df47fe9b140920025e7f367aff96
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-17 01:05:50 +0300

    Stack and queue homework

---
+++ TruckTour/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] pair = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int amountPetrol = pair[0];
+                int distance = pair[1];
+
+
+            }
+        }
+    }
+}
