94ad3d45e1bc8545aeeb7cb61ab1c565849b9810
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-17 00:41:35 +0300

    Queue and stack homework

---
+++ SongsQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+            Queue<string> queue = new Queue<string>(input);
+
+            while (queue.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command.Contains("Play"))
+                {
+                    queue.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    command = command.Replace("Add ", "");
+
+                    if (queue.Contains(command))
+                    {
+                        Console.WriteLine($"{command} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(command);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(", ", queue));
+                }
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("No more songs!");
+            }
+        }
+    }
+}
