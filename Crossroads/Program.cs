a36a7de0d13ca4db7eac1b9e68e15c6d8ecc1774
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-18 21:25:21 +0300

    Queue and stack homework

---
+++ Crossroads/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+
+            int greenCopy = greenLight;
+            int freeCopy = freeWindow;
+
+            Queue<string> queue = new Queue<string>();
+            int passedCars = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "green")
+                {
+                    int currQueueCount = queue.Count;
+
+                    for (int i = 0; i < currQueueCount; i++)
+                    {
+                        string currCar = queue.Peek();
+
+                        if (greenCopy - queue.Peek().Length >= 0)
+                        {
+                            greenCopy -= queue.Dequeue().Length;
+                            passedCars++;
+                        }
+                        else
+                        {
+                            if (queue.Peek().Length - greenCopy <= freeCopy)
+                            {
+                                freeCopy -= queue.Dequeue().Length - greenCopy;
+                                greenCopy = 0;
+                                passedCars++;
+                            }
+                            else
+                            {
+                                freeCopy -= queue.Peek().Length - greenCopy;
+                            }
+                        }
+
+                        if (freeCopy < 0)
+                        {
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{queue.Peek()} was hit at {queue.Peek()[queue.Peek().Length - greenCopy - 1]}.");
+                            return;
+                        }
+                    }
+
+                    greenCopy = greenLight;
+                    freeCopy = freeWindow;
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{passedCars} total cars passed the crossroads.");
+        }
+    }
+}
