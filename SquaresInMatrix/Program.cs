457f3bfe002d7f68616a15ebdb776d425e02a4b4
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-19 21:02:02 +0300

    Multidimensional Arrays - Homework

---
+++ SquaresInMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeMatrix = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            char[,] matrix = new char[sizeMatrix[0], sizeMatrix[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] currRow = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            int count = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row + 1, col] == matrix[row, col + 1] &&
+                        matrix[row, col + 1] == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
