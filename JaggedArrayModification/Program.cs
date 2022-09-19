0458e58214f9ad53edd5dc673b62810ac9cc57dc
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-19 16:43:31 +0300

    Multidimensional Arrays - Lab

---
+++ JaggedArrayModification/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace JaggedArrayModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[int.Parse(Console.ReadLine())][];
+
+            for (int rows = 0; rows < jaggedArray.GetLength(0); rows++)
+            {
+                int[] currRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                jaggedArray[rows] = new int[currRow.Length];
+
+                for (int cols = 0; cols < currRow.Length; cols++)
+                {
+                    jaggedArray[rows][cols] = currRow[cols];
+                }
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] spl = command.Split();
+                int row = int.Parse(spl[1]);
+                int col = int.Parse(spl[2]);
+                int value = int.Parse(spl[3]);
+
+                // validate coordinates
+                if (row < 0 || col < 0 || row >= jaggedArray.GetLength(0) || col >= jaggedArray[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (spl[0] == "Add")
+                {
+                    jaggedArray[row][col] += value;
+                }
+                else
+                {
+                    jaggedArray[row][col] -= value;
+                }
+            }
+
+            for (int row = 0; row < jaggedArray.GetLength(0); row++)
+            {
+                Console.WriteLine(string.Join(' ', jaggedArray[row]));
+            }
+        }
+    }
+}
