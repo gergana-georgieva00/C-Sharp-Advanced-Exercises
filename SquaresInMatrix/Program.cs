2fdf40143ba4a6ce1660fa770bc58da14a2135ed
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-19 21:18:40 +0300

    Multidimensional Arrays - Homework

--- SquaresInMatrix/Program.cs
+++ SquaresInMatrix/Program.cs
@@ -7,14 +7,14 @@ namespace SquaresInMatrix
     {
         static void Main(string[] args)
         {
-            int[] sizeMatrix = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] sizeMatrix = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             char[,] matrix = new char[sizeMatrix[0], sizeMatrix[1]];
 
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            for (int row = 0; row < sizeMatrix[0]; row++)
             {
-                char[] currRow = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                char[] currRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
 
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                for (int col = 0; col < sizeMatrix[1]; col++)
                 {
                     matrix[row, col] = currRow[col];
                 }
@@ -26,9 +26,9 @@ namespace SquaresInMatrix
             {
                 for (int col = 0; col < matrix.GetLength(1) - 1; col++)
                 {
-                    if (matrix[row, col] == matrix[row + 1, col] &&
-                        matrix[row + 1, col] == matrix[row, col + 1] &&
-                        matrix[row, col + 1] == matrix[row + 1, col + 1])
+                    if (matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col + 1] == matrix[row + 1, col] &&
+                        matrix[row + 1, col] == matrix[row + 1, col + 1])
                     {
                         count++;
                     }
