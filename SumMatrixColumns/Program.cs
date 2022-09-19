7fd9345cc8a86f583f2b448448ff28fa260a670c
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-19 14:21:06 +0300

    Multidimensional Arrays - Lab

---
+++ SumMatrixColumns/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeMatrix = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[sizeMatrix[0], sizeMatrix[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int currSum = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    currSum += matrix[row, col];
+                }
+
+                Console.WriteLine(currSum);
+            }
+        }
+    }
+}
