3999f539bf9743e1fff805d5383761962cc0b661
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-19 15:06:48 +0300

    Multidimensional Arrays - Lab

---
+++ SymbolInMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string currRow = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+        }
+    }
+}
