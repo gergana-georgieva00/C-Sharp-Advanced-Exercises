8bdd9af4d6c889f8498872be2b73fe6fb2b59e54
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-19 14:58:54 +0300

    Multidimensional Arrays - Lab

---
+++ PrimaryDiagonal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRoww = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRoww[col];
+                }
+            }
+
+            int currRow = 0;
+            int currCol = 0;
+            int sum = 0;
+
+            while (currRow < n && currCol < n)
+            {
+                sum += matrix[currRow, currCol];
+
+                currRow++;
+                currCol++;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
