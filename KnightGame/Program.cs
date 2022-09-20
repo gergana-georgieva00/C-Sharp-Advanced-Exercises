512de9e6adeaebb6334666a4b66514b6ab570082
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-20 18:11:28 +0300

    Multidimensional Arrays - Homework

---
+++ KnightGame/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 3)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string currRow = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            int knightsRemoved = 0;
+
+            while (true)
+            {
+                int countMostAttacking = 0;
+                int rowMostAtt = 0;
+                int colMostAtt = 0;
+
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (matrix[row, col] == 'K')
+                        {
+                            int attackedKnights = CountAttackedKnights(row, col, n, matrix);
+
+                            if (countMostAttacking < attackedKnights)
+                            {
+                                countMostAttacking = attackedKnights;
+                                rowMostAtt = row;
+                                colMostAtt = col;
+                            }
+                        }
+                    }
+                }
+
+                if (countMostAttacking == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    matrix[rowMostAtt, colMostAtt] = '0';
+                    knightsRemoved++;
+                }
+            }
+
+            Console.WriteLine(knightsRemoved);
+        }
+
+        private static int CountAttackedKnights(int row, int col, int size, char[,] matrix)
+        {
+            int attackedKnights = 0;
+
+            // horizontal left - up
+            if (IsCellValid(row - 1, col - 2, size))
+            {
+                if (matrix[row - 1, col - 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            // horizontal left - down
+            if (IsCellValid(row + 1, col - 2, size))
+            {
+                if (matrix[row + 1, col - 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            // horizontal right - up
+            if (IsCellValid(row - 1, col + 2, size))
+            {
+                if (matrix[row - 1, col + 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            // horizontal right - down
+            if (IsCellValid(row + 1, col + 2, size))
+            {
+                if (matrix[row + 1, col + 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            // vertical up-left
+            if (IsCellValid(row - 2, col - 1, size))
+            {
+                if (matrix[row - 2, col - 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            // vertical up-right
+            if (IsCellValid(row - 2, col + 1, size))
+            {
+                if (matrix[row - 2, col + 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            // vertical down-left
+            if (IsCellValid(row + 2, col - 1, size))
+            {
+                if (matrix[row + 2, col - 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            // vertical down-right
+            if (IsCellValid(row + 2, col + 1, size))
+            {
+                if (matrix[row + 2, col + 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            return attackedKnights;
+        }
+
+        static bool IsCellValid(int row, int col, int size)
+        {
+            return row >= 0 && row < size && col >= 0 && col < size;
+        }
+    }
+}
