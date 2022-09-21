d7040a6dd08217f97cc451c9d2eda755516a3bdb
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-21 13:30:22 +0300

    Sets and Dictionaries Advanced - Lab

---
+++ Largest3Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            integers = integers.OrderByDescending(i => i).ToArray();
+
+            if (integers.Length <= 3)
+            {
+                Console.WriteLine(string.Join(' ', integers));
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(integers[i] + " ");
+                }
+            }
+        }
+    }
+}
