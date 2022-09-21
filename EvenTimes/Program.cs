0cc9a5fb68d998c03743c98e19b975d96aed6c06
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-21 19:16:11 +0300

    Sets and Dictionaries Advanced - Homework

---
+++ EvenTimes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numberCount = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!numberCount.ContainsKey(num))
+                {
+                    numberCount.Add(num, 1);
+                }
+                else
+                {
+                    numberCount[num]++;
+                }
+            }
+
+            foreach (var kvp in numberCount)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
