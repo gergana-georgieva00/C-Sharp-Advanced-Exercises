096171a63187bf7de8b84286ed81134a299400fd
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-21 17:44:18 +0300

    Sets and Dictionaries Advanced - Lab

---
+++ ParkingLot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] spl = input.Split(", ");
+
+                string direction = spl[0];
+                string carNumber = spl[1];
+
+                if (direction == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else
+                {
+                    cars.Remove(carNumber);
+                }
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, cars));
+        }
+    }
+}
