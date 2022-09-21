93c757c3485ee9780df8082288612cfbea86abec
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-21 17:20:13 +0300

    Sets and Dictionaries Advanced - Lab

---
+++ CitiesByContinent AndCountry/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> countries = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!countries.ContainsKey(continent))
+                {
+                    countries.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!countries[continent].ContainsKey(country))
+                {
+                    countries[continent].Add(country, new List<string>());
+                }
+
+                countries[continent][country].Add(city);
+               
+            }
+
+            foreach (var kvp in countries)
+            {
+                Console.WriteLine($"{kvp.Key}:");
+
+                foreach (var countryCity in kvp.Value)
+                {
+                    Console.WriteLine($"  {countryCity.Key} -> {string.Join(", ", countryCity.Value)}");
+                }
+            }
+        }
+    }
+}
