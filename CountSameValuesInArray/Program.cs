641c240c1f2e040e5c0ae370b1ad372d3c8ee1b2
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-21 12:54:28 +0300

    Sets and Dictionaries Advanced - Lab

---
+++ CountSameValuesInArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> valuesCount = new Dictionary<double, int>();
+
+            double[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            foreach (var value in input)
+            {
+                if (valuesCount.ContainsKey(value))
+                {
+                    valuesCount[value]++;
+                }
+                else
+                {
+                    valuesCount.Add(value, 1);
+                }
+            }
+
+            foreach (var kvp in valuesCount)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
