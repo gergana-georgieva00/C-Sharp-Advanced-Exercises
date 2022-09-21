931023483e57a8937465e5e94322423f1a60a406
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-21 19:22:25 +0300

    Sets and Dictionaries Advanced - Homework

---
+++ CountSymbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> chars = new Dictionary<char, int>();
+
+            foreach (var character in input)
+            {
+                if (!chars.ContainsKey(character))
+                {
+                    chars.Add(character, 1);
+                }
+                else
+                {
+                    chars[character]++;
+                }
+            }
+
+            foreach (var kvp in chars.OrderBy(c => c.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
