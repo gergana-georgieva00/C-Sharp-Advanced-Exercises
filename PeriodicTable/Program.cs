5bc1684138c540b979d3c6c594821a997e539cb2
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-21 19:05:01 +0300

    Sets and Dictionaries Advanced - Homework

---
+++ PeriodicTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> elements = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in input)
+                {
+                    elements.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', elements.OrderBy(e => e)));
+        }
+    }
+}
