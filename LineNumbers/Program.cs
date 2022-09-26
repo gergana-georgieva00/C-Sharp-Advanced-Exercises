00b41326c841eca9f662f7b69d021fcb2c101d7a
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-26 19:09:55 +0300

    Streams, Files and Directories - Lab

---
+++ LineNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            // TODO: write your code here…
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int num = 1;
+
+                    while (!reader.EndOfStream)
+                    {
+                        string currLine = reader.ReadLine();
+
+                        writer.WriteLine($"{num.ToString()}. {currLine}");
+
+                        num++;
+                    }
+                }
+            }
+        }
+    }
+}
