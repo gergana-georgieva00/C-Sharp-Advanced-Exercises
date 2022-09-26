e6e69ed732642c16b6f7f9a43ea566314ae045fc
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-26 13:29:02 +0300

    Streams, Files and Directories - Lab

---
+++ OddLInes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            // TODO: write your code here…
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int currRow = 0;
+
+                    while (!reader.EndOfStream)
+                    {
+                        string currLine = reader.ReadLine();
+
+                        if (currRow % 2 == 1)
+                        {
+                            writer.WriteLine(currLine);
+                        }
+
+                        currRow++;
+                    }
+                }
+            }
+        }
+    }
+}
