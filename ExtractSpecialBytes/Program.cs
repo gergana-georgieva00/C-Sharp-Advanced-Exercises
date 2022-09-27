09339940623e66c4f24ba80bac3193c94f1eb8e5
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-27 15:03:27 +0300

    Streams, Files and Directories - Lab

---
+++ ExtractSpecialBytes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+
+namespace ExtractSpecialBytes
+{
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            // TODO: write your code here…
+
+            using (FileStream image = new FileStream(binaryFilePath, FileMode.Open))
+            {
+                using (FileStream bytes = new FileStream(bytesFilePath, FileMode.Open))
+                {
+                    byte[] bytesBuffer = new byte[bytes.Length];
+                    bytes.Read(bytesBuffer, 0, (int)bytes.Length);
+
+                    byte[] imageBuffer = new byte[image.Length];
+                    bytes.Read(imageBuffer, 0, (int)image.Length);
+
+                    using (FileStream output = new FileStream(outputPath, FileMode.Open))
+                    {
+                        for (int i = 0; i < imageBuffer.Length; i++)
+                        {
+                            for (int j = 0; j < bytesBuffer.Length; j++)
+                            {
+                                if (imageBuffer[i] == bytesBuffer[j])
+                                {
+                                    output.Write(new byte[] { imageBuffer[i] });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
