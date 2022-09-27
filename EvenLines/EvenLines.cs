0c6070823aafe53f1c34d357429b220b0886c5f4
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-27 18:27:18 +0300

    Streams, Files and Directories - Exercises

---
+++ EvenLines/EvenLines.cs
@@ -0,0 +1,51 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int counter = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    string currLine = reader.ReadLine();
+                    currLine = currLine.Replace('-', '@');
+                    currLine = currLine.Replace(',', '@');
+                    currLine = currLine.Replace('.', '@');
+                    currLine = currLine.Replace('!', '@');
+                    currLine = currLine.Replace('?', '@');
+
+                    string[] words = currLine.Split();
+
+                    if (counter % 2 == 0)
+                    {
+                        for (int i = words.Length - 1; i >= 0; i--)
+                        {
+                            sb.Append(words[i] + " ");
+                        }
+
+                        sb.AppendLine();
+                    }
+
+                    counter++;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
