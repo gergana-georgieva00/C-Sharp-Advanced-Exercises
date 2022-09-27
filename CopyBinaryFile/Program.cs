ebcde7bf9c83fddec519d31c898560dba30d67e4
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-27 20:03:29 +0300

    Streams, Files and Directories - Exercises

---
+++ CopyBinaryFile/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream reader = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (FileStream writer = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[reader.Length];
+                    reader.Read(buffer, 0, (int)reader.Length);
+
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        writer.Write(new byte[] { buffer[i] });
+                    }
+                }
+            }
+        }
+    }
+}
