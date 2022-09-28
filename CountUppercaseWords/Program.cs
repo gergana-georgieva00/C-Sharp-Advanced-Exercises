b586819a6777be944fb2959941166587ca445202
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-28 15:42:53 +0300

    Functional Programming - Lab

---
+++ CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> isStartWithCapital = w => Char.IsUpper(w[0]);
+
+            input = input.Where(x => isStartWithCapital(x)).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, input));
+        }
+    }
+}
