e3c0f5d912b9f34c027dffbf068ac26d33302a98
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-28 15:31:54 +0300

    Functional Programming - Lab

---
+++ SortEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            integers = integers.Where(n => n % 2 == 0).OrderBy(n => n).ToList();
+            Console.WriteLine(string.Join(", ", integers));
+        }
+    }
+}
