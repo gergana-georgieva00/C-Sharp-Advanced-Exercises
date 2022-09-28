598623e5fbbbddc7e089f0189fbae548a600d182
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-28 15:55:25 +0300

    Functional Programming - Lab

---
+++ AddVAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, decimal> parseToDouble = d => decimal.Parse(d);
+            List<decimal> prices = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(parseToDouble).ToList();
+
+            Func<decimal, decimal> addVAT = d => d += 0.2m * d;
+            prices = prices.Select(addVAT).ToList();
+            prices.ForEach(p => Console.WriteLine($"{p:f2}"));
+        }
+    }
+}
