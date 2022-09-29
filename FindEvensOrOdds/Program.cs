a70e89d61de318db57786a9786663a72d70c8df7
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-29 15:34:42 +0300

    Functional Programming - Exercise

---
+++ FindEvensOrOdds/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isInRange = num => num % 2 == 0;
+
+            int[] range = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string evenOrOdd = Console.ReadLine();
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                if (evenOrOdd == "even")
+                {
+                    if (isInRange(i))
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+                else
+                {
+                    if (!isInRange(i))
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
