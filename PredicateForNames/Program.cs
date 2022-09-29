eec538411f64e584894fb17db5e3ecb3b5c117ea
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-29 19:49:57 +0300

    Functional Programming - Exercise

---
+++ PredicateForNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Func<string, bool> filter = name => name.Length <= n;
+            names = names.Where(name => filter(name)).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
