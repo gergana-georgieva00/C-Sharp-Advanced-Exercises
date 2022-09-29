8ebbeca8f51d61c04982432634852994c5fcf05c
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-29 15:07:51 +0300

    Functional Programming - Exercise

---
+++ ActionPoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Action<List<string>> printer = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+            printer(input);
+        }
+    }
+}
