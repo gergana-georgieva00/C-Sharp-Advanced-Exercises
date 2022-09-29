beeb129f32d1c2eab8e347330c32458f182f17bf
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-29 15:22:43 +0300

    Functional Programming - Exercise

---
+++ KnightsOfHonor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Action<string> printer = x => Console.WriteLine("Sir " + x);
+            names.ForEach(n => printer(n));
+        }
+    }
+}
