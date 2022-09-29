f67a536bdebddc24f3d30eb2f7e9f46876f1bc13
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-29 22:21:31 +0300

    Functional Programming - Exercise

---
+++ ListOfPredicates/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> dividers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            List<int> numsToCheck = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                numsToCheck.Add(i);
+            }
+
+            Func<int, int, bool> checker = (num1, num2) => num1 % num2 == 0;
+
+            foreach (var numToCheck in numsToCheck)
+            {
+                bool isValid = false;
+
+                foreach (var divider in dividers)
+                {
+                    if (checker(numToCheck, divider))
+                    {
+                        isValid = true;
+                    }
+                    else
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (!isValid)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.Write(numToCheck + " ");
+                }
+            }
+        }
+    }
+}
