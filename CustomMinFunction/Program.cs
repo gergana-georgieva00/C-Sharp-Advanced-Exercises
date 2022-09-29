8b377c3183831eb6cd2093e6fef559c7d404a504
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-29 15:26:50 +0300

    Functional Programming - Exercise

---
+++ CustomMinFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Func<List<int>, int> findSmallestNum = x => x.Min();
+            Action<List<int>> printer = x => Console.WriteLine(findSmallestNum(x));
+            printer(integers);
+        }
+    }
+}
