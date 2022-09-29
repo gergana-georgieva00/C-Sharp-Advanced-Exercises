83f0bc2cfae6e8fbae414302336f1dad860bce54
Gergana Georgieva <gerganagergana1@gmail.com>
2022-09-29 19:42:17 +0300

    Functional Programming - Exercise

---
+++ ReverseAndExclude/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int nFilter = int.Parse(Console.ReadLine());
+
+            Func<int, bool> filter = n => n % nFilter != 0;
+            numbers = numbers.Where(n => filter(n)).Reverse().ToList();
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
