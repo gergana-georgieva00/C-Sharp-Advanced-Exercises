ca9dbb2f36a9da2a11f556ae737edc3fa860ce3e
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-01 18:43:28 +0300

    Defining Classes - Lab

--- CarManufacturer/Car.cs
+++ CarManufacturer/Car.cs
@@ -11,6 +11,8 @@ namespace CarManufacturer
         private int year;
         private double fuelQuantity;
         private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
 
         public Car()
         {
@@ -36,11 +38,20 @@ namespace CarManufacturer
             this.FuelConsumption = fuelConsumption;
         }
 
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
         public double FuelQuantity { get; set; }
         public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
 
         public void Drive(double distance)
         {
