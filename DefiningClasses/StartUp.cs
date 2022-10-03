9fa916b5748b3959f1a8506ac7d5f85d26cb2712
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-03 15:40:19 +0300

    Defining Classes - Exercise

---
+++ DefiningClasses/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person peter = new Person();
+            peter.Name = "Peter";
+            peter.Age = 20;
+            Console.WriteLine(peter.Name);
+            Console.WriteLine(peter.Age);
+        }
+    }
+}
