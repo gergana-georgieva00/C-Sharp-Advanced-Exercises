7cf44522904fc31a3ad7a5bb4993391ef12f6e9d
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-03 16:31:12 +0300

    Defining Classes - Exercise

--- DefiningClasses/Person.cs
+++ DefiningClasses/Person.cs
@@ -9,6 +9,24 @@ namespace DefiningClasses
         private string name;
         private int age;
 
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age)
+            : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+            : this(age)
+        {
+            this.Name = name;
+        }
+
         public string Name { get { return name; } set { name = value; } }
         public int Age { get { return age; } set { age = value; } }
     }
--- DefiningClasses/StartUp.cs
+++ DefiningClasses/StartUp.cs
@@ -6,11 +6,7 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            Person peter = new Person();
-            peter.Name = "Peter";
-            peter.Age = 20;
-            Console.WriteLine(peter.Name);
-            Console.WriteLine(peter.Age);
+
         }
     }
 }
