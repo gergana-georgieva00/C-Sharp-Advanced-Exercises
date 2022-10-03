096fd2884df12150f9149477fd2ca6ab99943c2c
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-03 17:00:04 +0300

    Defining Classes - Exercise

---
+++ DefiningClasses/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public double GetDifferenceInDays(string startDate, string endDate)
+        {
+            int[] stDate = startDate.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] enDate = endDate.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            DateTime date1 = new DateTime(stDate[0], stDate[1], stDate[2]);
+            DateTime date2 = new DateTime(enDate[0], enDate[1], enDate[2]);
+
+            return (date2 - date1).TotalDays;
+        }
+    }
+}
--- DefiningClasses/StartUp.cs
+++ DefiningClasses/StartUp.cs
@@ -7,27 +7,34 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            //int n = int.Parse(Console.ReadLine());
 
-            Family family = new Family();
+            //Family family = new Family();
 
-            for (int i = 0; i < n; i++)
-            {
-                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            //for (int i = 0; i < n; i++)
+            //{
+            //    string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-                string name = input[0];
-                int age = int.Parse(input[1]);
+            //    string name = input[0];
+            //    int age = int.Parse(input[1]);
 
-                Person person = new Person(name, age);
-                family.AddMember(person);
-            }
+            //    Person person = new Person(name, age);
+            //    family.AddMember(person);
+            //}
 
-            var sorted = family.People.Where(p => p.Age > 30).OrderBy(p => p.Name).ToList();
+            //var sorted = family.People.Where(p => p.Age > 30).OrderBy(p => p.Name).ToList();
 
-            foreach (var person in sorted)
-            {
-                Console.WriteLine($"{person.Name} - {person.Age}");
-            }
+            //foreach (var person in sorted)
+            //{
+            //    Console.WriteLine($"{person.Name} - {person.Age}");
+            //}
+
+
+            string date1 = Console.ReadLine();
+            string date2 = Console.ReadLine();
+
+            DateModifier modifier = new DateModifier();
+            Console.WriteLine(Math.Abs(modifier.GetDifferenceInDays(date1, date2)));
         }
     }
 }
