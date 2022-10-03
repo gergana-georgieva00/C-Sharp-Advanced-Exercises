3d9acb11b95cd3a0a931f3cf6817dcba5ed5c4cf
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-03 16:40:06 +0300

    Defining Classes - Exercise

---
+++ DefiningClasses/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person member)
+        {
+            this.People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            List<Person> sorted = this.People.OrderByDescending(p => p.Age).ToList();
+            return sorted[0];
+        }
+    }
+}
--- DefiningClasses/StartUp.cs
+++ DefiningClasses/StartUp.cs
@@ -6,7 +6,22 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Console.WriteLine(family.GetOldestMember().Name + " " + family.GetOldestMember().Age);
         }
     }
 }
