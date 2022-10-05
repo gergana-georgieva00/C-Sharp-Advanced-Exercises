7d020bb16c965acd6bd6d075082263530185e9bb
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-05 16:03:41 +0300

    C# Advanced Exam - 25 October 2020

---
+++ Classroom/ClassroomProject.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classroom
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        private int capacity;
+
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            this.Students = new List<Student>();
+        }
+
+        public List<Student> Students { get { return students; } set { students = value; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+        public int Count { get { return this.Students.Count; } }
+
+        public string RegisterStudent(Student student)
+        {
+            if (this.Students.Count < this.Capacity)
+            {
+                this.Students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+
+            return "No seats in the classroom";
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            if (this.Students.Any(s => s.FirstName == firstName))
+            {
+                this.Students = this.Students.Where(s => s.FirstName != firstName).ToList();
+                return $"Dismissed student {firstName} {lastName}";
+            }
+
+            return "Student not found";
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            List<Student> filtered = this.Students.Where(s => s.Subject == subject).ToList();
+
+            if (filtered.Count == 0)
+            {
+                return "No students enrolled for the subject";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Subject: {subject}");
+            sb.AppendLine("Students:");
+
+            foreach (var student in filtered)
+            {
+                sb.AppendLine($"{student.FirstName} {student.LastName}");
+            }
+
+            return sb.ToString();
+        }
+
+        public int GetStudentsCount()
+        {
+            return this.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = this.Students.Find(s => s.FirstName == firstName && s.LastName == lastName);
+
+            return student;
+        }
+    }
+}
