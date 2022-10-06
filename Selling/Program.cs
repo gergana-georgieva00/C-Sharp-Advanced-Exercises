0f19c16a4c336f29083013ce146330d910474475
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-06 22:34:11 +0300

    C# Advanced Retake Exam - 16 December 2020

--- Selling/Program.cs
+++ Selling/Program.cs
@@ -21,6 +21,7 @@ namespace Selling
             }
 
             int collectedMoney = 0;
+            bool enoughMoney = true;
 
             while (collectedMoney < 50)
             {
@@ -135,8 +136,31 @@ namespace Selling
                 else
                 {
                     Console.WriteLine("Bad news, you are out of the bakery.");
+                    enoughMoney = false;
                     break;
                 }
+            }
+
+            if (enoughMoney)
+            {
+                Console.WriteLine("Good news! You succeeded in collecting enough money!");
+            }
+
+            Console.WriteLine($"Money: {collectedMoney}");
+
+            PrintMatrix(matrix);
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
             }
         }
 
