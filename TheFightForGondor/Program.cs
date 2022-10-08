7fe38f7c57c1169e5efd1386fd1321a4657d78b1
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-08 22:54:16 +0300

    C# Advanced Exam - 20 February 2021

--- TheFightForGondor/Program.cs
+++ TheFightForGondor/Program.cs
@@ -13,6 +13,32 @@ namespace TheFightForGondor
             int[] platesInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
             Queue<int> plates = new Queue<int>(platesInput);
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] orcsInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                Stack<int> orcs = new Stack<int>(orcsInput);
+
+                while (orcs.Count > 0 && plates.Count > 0)
+                {
+                    if (orcs.Peek() > plates.Peek())
+                    {
+                        int currOrc = orcs.Pop() - plates.Dequeue();
+                        orcs.Push(currOrc);
+                    }
+                    else if (orcs.Peek() < plates.Peek())
+                    {
+                        List<int> newPlates = new List<int>(plates);
+                        newPlates[0] -= orcs.Pop();
+                        plates = new Queue<int>(newPlates);
+                    }
+                    else
+                    {
+                        orcs.Pop();
+                        plates.Dequeue();
+                    }
+                }
+            }
         }
     }
 }
