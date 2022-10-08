30f8c4f7db8562e36db91d984212eeeac386abe9
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-08 22:45:36 +0300

    C# Advanced Exam - 20 February 2021

---
+++ TheFightForGondor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheFightForGondor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] platesInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> plates = new Queue<int>(platesInput);
+        }
+    }
+}
