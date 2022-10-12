09bd20172b8d74006b6c4ebd93bec5ba7a660f89
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-12 22:13:55 +0300

    Generics - Exercise

---
+++ Tuple/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] first = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] second = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] third = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            MyTuple<string, string> firstT = new MyTuple<string, string>(first[0] + " " + first[1], first[2]);
+            MyTuple<string, int> secondT = new MyTuple<string, int>(second[0], int.Parse(second[1]));
+            MyTuple<int, double> thirdT = new MyTuple<int, double>(int.Parse(third[0]), double.Parse(third[1]));
+
+            Console.WriteLine(firstT.ToString());
+            Console.WriteLine(secondT.ToString());
+            Console.WriteLine(thirdT.ToString());
+        }
+    }
+}
