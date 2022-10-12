790d39ea2cb621088b92897a730a1bd94a2b0554
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-12 21:58:51 +0300

    Generics - Exercise

---
+++ GenericCountMethodDouble/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        public Box(T data)
+        {
+            this.Data = data;
+        }
+
+        public T Data { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Data.GetType()}: {this.Data}".ToString();
+        }
+
+        public bool AreEqual(T element)
+        {
+            if (this.Data.CompareTo(element) == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<double>> list = new List<Box<double>>();
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                list.Add(new Box<double>(input));
+            }
+
+            double elementForComparison = double.Parse(Console.ReadLine());
+
+            foreach (var item in list)
+            {
+                if (item.AreEqual(elementForComparison))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
