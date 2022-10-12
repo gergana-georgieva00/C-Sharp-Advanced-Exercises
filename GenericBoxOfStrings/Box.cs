cdf6517607f3486be40f9673dce34756fee2a47d
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-12 20:59:21 +0300

    Generics - Exercise

---
+++ GenericBoxOfStrings/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfStrings
+{
+    public class Box<T>
+    {
+        public Box(T input)
+        {
+            this.Input = input;
+        }
+        public T Input { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Input.GetType()}: {this.Input}";
+        }
+    }
+}
