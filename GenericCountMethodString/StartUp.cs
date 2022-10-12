f9a5ddf2f3ae11dd816fb7f030488ad63ff67f05
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-12 21:55:16 +0300

    Generics - Exercise

---
+++ GenericCountMethodString/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<string>> list = new List<Box<string>>();
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                list.Add(new Box<string>(input));
+            }
+
+            string elementForComparison = Console.ReadLine();
+
+            foreach (var item in list)
+            {
+                if (item.AreEqual(elementForComparison))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
