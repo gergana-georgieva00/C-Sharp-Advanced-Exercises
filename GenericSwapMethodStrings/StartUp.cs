1949f74ac6d038de9de4a1967d6f212abe3c5b5a
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-12 21:17:49 +0300

    Generics - Exercise

---
+++ GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            int[] indexes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int index1 = indexes[0];
+            int index2 = indexes[1];
+
+
+        }
+
+        static void Swap<T>(List<T> someList, int index1, int index2)
+        {
+
+        }
+    }
+}
