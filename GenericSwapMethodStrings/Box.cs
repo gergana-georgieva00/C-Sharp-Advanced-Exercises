e372ecfd0b0b6eeb7aca0571dc7010d714acf1eb
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-12 21:38:03 +0300

    Generics - Exercise

---
+++ GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    public class Box<T>
+    {
+        public Box(T data)
+        {
+            this.Data = data;
+        }
+
+        public T Data { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Data.GetType()}: {this.Data}".ToString();
+        }
+    }
+}
--- GenericSwapMethodStrings/StartUp.cs
+++ GenericSwapMethodStrings/StartUp.cs
@@ -10,23 +10,35 @@ namespace GenericSwapMethodStrings
         {
             int n = int.Parse(Console.ReadLine());
 
-            List<string> list = new List<string>();
+            List<Box<string>> list = new List<Box<string>>();
 
             for (int i = 0; i < n; i++)
             {
-                list.Add(Console.ReadLine());
+                list.Add(new Box<string>(Console.ReadLine()));
             }
 
             int[] indexes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             int index1 = indexes[0];
             int index2 = indexes[1];
 
+            Swap<string>(list, index1, index2);
 
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
 
-        static void Swap<T>(List<T> someList, int index1, int index2)
+        static void Swap<T>(List<Box<T>> list, int index1, int index2)
         {
+            var removed1 = list[index1];
+            var removed2 = list[index2];
 
+            list.RemoveAt(index1);
+            list.Insert(index1, removed2);
+
+            list.RemoveAt(index2);
+            list.Insert(index2, removed1);
         }
     }
 }
