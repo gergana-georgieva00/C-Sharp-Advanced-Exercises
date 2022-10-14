22860a48b82721b38b506473e4af51d9f5688c18
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-14 23:06:34 +0300

    Iterators and Comparators - Exercise

---
+++ Stack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+
+            Stackk<int> stack = new Stackk<int>(input);
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command.Contains("Push"))
+                {
+                    int numToPush = int.Parse(command.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
+                    stack.Push(numToPush);
+                }
+                else
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class Stackk<T> : IEnumerable<T>
+    {
+        private List<T> stack;
+
+        public Stackk(List<T> collection)
+        {
+            this.stack = new List<T>(collection);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.stack.Count - 1; i >= 0; i--)
+            {
+                yield return this.stack[i];
+            }
+
+            for (int i = this.stack.Count - 1; i >= 0; i--)
+            {
+                yield return this.stack[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Push(T element)
+        {
+            this.stack.Add(element);
+        }
+
+        public T Pop()
+        {
+            if (this.stack.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            T removed = this.stack[this.stack.Count - 1];
+            this.stack.RemoveAt(this.stack.Count - 1);
+
+            return removed;
+        }
+    }
+}
