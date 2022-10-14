69a56dd9fb5ac9b3de8559d38bd2d79d40cda7f3
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-14 16:32:56 +0300

    Iterators and Comparators - Exercise

---
+++ ListyIterator/ListyIterator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        int index = 0;
+
+        public ListyIterator(List<T> collection)
+        {
+            this.collection = new List<T>(collection);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                yield return collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public bool Move()
+        {
+            if (index + 1 < collection.Count)
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < collection.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Pperation!");
+            }
+
+            Console.WriteLine(collection[index]);
+        }
+    }
+}
