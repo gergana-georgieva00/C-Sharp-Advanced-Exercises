9c72bd0d0e69bb7aee01dc4248944cf5206f2465
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-22 09:12:02 +0300

    C# Advanced Exam - 22 October 2022

---
+++ EnergyDrinks/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnergyDrinks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> mgCoffee = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            Queue<int> energyDrinks = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            int caffeinePerDay = 0;
+
+            while (mgCoffee.Count > 0 && energyDrinks.Count > 0)
+            {
+                int currMult = mgCoffee.Peek() * energyDrinks.Peek();
+
+                if (currMult + caffeinePerDay <= 300)
+                {
+                    mgCoffee.Pop();
+                    energyDrinks.Dequeue();
+
+                    caffeinePerDay += currMult;
+                }
+                else
+                {
+                    mgCoffee.Pop();
+                    energyDrinks.Enqueue(energyDrinks.Dequeue());
+
+                    if (caffeinePerDay - 30 >= 0)
+                    {
+                        caffeinePerDay -= 30;
+                    }
+                    else
+                    {
+                            
+                    }
+                }
+            }
+
+            if (energyDrinks.Count > 0)
+            {
+                Console.WriteLine($"Drinks left: {string.Join(", ", energyDrinks)}");
+            }
+            else
+            {
+                Console.WriteLine("At least Stamat wasn't exceeding the maximum caffeine.");
+            }
+
+            Console.WriteLine($"Stamat is going to sleep with {caffeinePerDay} mg caffeine.");
+        }
+    }
+}
---
+++ WallDestroyer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WallDestroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+            int currRow = 0;
+            int currCol = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                string rowInput = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+
+                    if (matrix[row, col] == 'V')
+                    {
+                        currRow = row;
+                        currCol = col;
+                    }
+                }
+            }
+
+            // recieve commands
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+
+            }
+        }
+    }
+}
