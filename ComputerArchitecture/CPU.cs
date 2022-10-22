86a75a48233ca7d699718502b708418f2d650d55
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-22 09:34:06 +0300

    C# Advanced Exam - 22 October 2022

---
+++ ComputerArchitecture/CPU.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class CPU
+    {
+        public CPU(string brand, int cores, double frequency)
+        {
+            this.Brand = brand;
+            this.Cores = cores;
+            this.Frequency = frequency;
+        }
+
+        public string Brand { get; set; }
+        public int Cores { get; set; }
+        public double Frequency { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Brand} CPU:" + Environment.NewLine + $"Cores: {this.Cores}" + Environment.NewLine + $"Frequency: {this.Frequency} GHz".ToString();
+        }
+    }
+}
---
+++ ComputerArchitecture/Computer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        private List<CPU> multiprocessor;
+
+        public Computer(string model, int capacity)
+        {
+            this.Model = model;
+            this.Capacity = capacity;
+            this.Multiprocessor = new List<CPU>();
+        }
+
+        public List<CPU> Multiprocessor { get; set; }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return this.Multiprocessor.Count; } }
+
+        public void Add(CPU cpu)
+        {
+            if (this.Multiprocessor.Count < this.Capacity)
+            {
+                this.Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            if (this.Multiprocessor.Any(p => p.Brand == brand))
+            {
+                this.Multiprocessor.Remove(this.Multiprocessor.Find(p => p.Brand == brand));
+                return true;
+            }
+
+            return false;
+        }
+
+        public CPU MostPowerful()
+        {
+            CPU mostPowerful = this.Multiprocessor.OrderByDescending(p => p.Frequency).ToList()[0];
+            return mostPowerful;
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            if (this.Multiprocessor.Any(p => p.Brand == brand))
+            {
+                CPU cpu = this.Multiprocessor.Find(p => p.Brand == brand);
+                return cpu;
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            return $"CPUs in the Computer {this.Model}:" + Environment.NewLine + string.Join(Environment.NewLine, this.Multiprocessor);
+        }
+    }
+}
