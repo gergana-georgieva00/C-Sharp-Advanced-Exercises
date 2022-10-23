10832677ee32f929d94a4cf7e71f7688aa7334f7
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-23 21:14:29 +0300

    Basic Algorithms - Exercise

---
+++ RecursiveFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Result(n));
+        }
+
+        static long Result(int n)
+        {
+            long result = 0;
+
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return result += n * Result(n - 1);
+        }
+    }
+}
