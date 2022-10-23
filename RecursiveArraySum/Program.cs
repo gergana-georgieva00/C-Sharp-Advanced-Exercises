e5e15f12bfc70b11420ed3babbad28a96ba550ba
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-23 21:10:50 +0300

    Basic Algorithms - Exercise

---
+++ RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int index = 0;
+            Console.WriteLine(Sum(integers, index));
+        }
+
+        static int Sum(int[] array, int index)
+        {
+            int sum = 0;
+
+            if (index < array.Length)
+            {
+                return sum += array[index++] + Sum(array, index);
+            }
+
+            return sum;
+        }
+    }
+}
