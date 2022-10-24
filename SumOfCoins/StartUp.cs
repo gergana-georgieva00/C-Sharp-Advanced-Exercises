a469b2f0f583020c997aaed8dce0ba718ee870e1
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-24 17:29:30 +0300

    Basic Algorithms - Exercise

---
+++ SumOfCoins/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumOfCoins
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine().Split(", ").Select(int.Parse).OrderByDescending(i => i).ToArray();
+            int sum = int.Parse(Console.ReadLine());
+
+
+        }
+
+        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+        {
+            return null;
+        }
+    }
+}
