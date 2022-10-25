c0ceef04378f79e485cda53aba947af6a058f990
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-25 19:50:15 +0300

    Basic Algorithms - Exercise

--- SumOfCoins/StartUp.cs
+++ SumOfCoins/StartUp.cs
@@ -8,15 +8,52 @@ namespace SumOfCoins
     {
         static void Main(string[] args)
         {
-            int[] integers = Console.ReadLine().Split(", ").Select(int.Parse).OrderByDescending(i => i).ToArray();
+            var integers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
             int sum = int.Parse(Console.ReadLine());
 
+            var selectedCoins = ChooseCoins(integers, sum);
 
+            Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+
+            foreach (var selectedCoin in selectedCoins)
+            {
+                Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
+            }
         }
 
         public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
         {
-            return null;
+            int[] sortedCoins = coins.OrderByDescending(c => c).ToArray();
+
+            Dictionary<int, int> chosenCoins = new Dictionary<int, int>();
+
+            int currentSum = 0;
+            int coinIndex = 0;
+
+            while (currentSum != targetSum && coinIndex < sortedCoins.Length)
+            {
+                int currCoinValue = sortedCoins[coinIndex];
+                int remainder = targetSum - currentSum;
+                int numberOfCoins = remainder / currCoinValue;
+
+                if (currentSum + currCoinValue <= targetSum)
+                {
+                    chosenCoins.Add(currCoinValue, numberOfCoins);
+                    currentSum += currCoinValue;
+                }
+
+                coinIndex++;
+            }
+
+            if (currentSum != targetSum)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                return chosenCoins;
+            }
+            
         }
     }
 }
